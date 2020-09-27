90a010e4dc99b44f548eb7afddbb74910be5cd9b
rubi16 <april16rubi@gmail.com>
2020-09-27 20:56:10 +0530

    seccond commit

---
+++ faculty.js
@@ -0,0 +1,28 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.etm_headline .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.etm_headline .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.etm_headline .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: 'etm_headline',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
